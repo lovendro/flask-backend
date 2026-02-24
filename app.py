64e0daf42fe1980a814ae705d4b91ca3c4bc95e7
lovendro <lovendro.p@gmail.com>
2026-02-24 15:35:11 +0300

    changed account reference

--- app.py
+++ app.py
@@ -188,7 +188,7 @@ def mpesa_payment():
         "PartyB": "174379",
         "PhoneNumber": phone,
         "CallBackURL": "https://coding.co.ke/api/confirm.php",
-        "AccountReference": " chui SokoGarden Online",
+        "AccountReference": " farasi SokoGarden Online",
         "TransactionDesc": "Payments for Products"
         }
 
